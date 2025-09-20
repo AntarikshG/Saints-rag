324b450ab10955c2425adea67863d2ce269393ec
Antariksh <antariksh.bhardwaj@gmail.com>
2025-09-20 10:20:39 +0200

    First push

---
+++ lib/l10n/app_localizations_hi.dart
@@ -0,0 +1,82 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hindi (`hi`).
+class AppLocalizationsHi extends AppLocalizations {
+  AppLocalizationsHi([String locale = 'hi']) : super(locale);
+
+  @override
+  String get appTitle => 'प्रेरणादायक संत';
+
+  @override
+  String get inspiringSaints => 'भारत के प्रेरणादायक संत';
+
+  @override
+  String get contact => 'संपर्क करें';
+
+  @override
+  String get contactUs => 'हमसे संपर्क करें: example@email.com';
+
+  @override
+  String get selectTheme => 'थीम चुनें';
+
+  @override
+  String get chooseTheme => 'थीम चुनें';
+
+  @override
+  String get system => 'सिस्टम';
+
+  @override
+  String get light => 'हल्का';
+
+  @override
+  String get dark => 'गहरा';
+
+  @override
+  String get setName => 'नाम सेट करें';
+
+  @override
+  String get enterYourName => 'अपना नाम दर्ज करें';
+
+  @override
+  String get name => 'नाम';
+
+  @override
+  String get save => 'सहेजें';
+
+  @override
+  String get quotes => 'उद्धरण';
+
+  @override
+  String get articles => 'लेख';
+
+  @override
+  String get ask => 'प्रश्न पूछें';
+
+  @override
+  String get history => 'इतिहास';
+
+  @override
+  String get askAQuestion => 'प्रश्न पूछें';
+
+  @override
+  String get answer => 'उत्तर';
+
+  @override
+  String get noPreviousQuestions => 'कोई पिछले प्रश्न नहीं।';
+
+  @override
+  String get menu => 'मेनू';
+
+  @override
+  String get language => 'भाषा';
+
+  @override
+  String get english => 'अंग्रेज़ी';
+
+  @override
+  String get hindi => 'हिंदी';
+}
